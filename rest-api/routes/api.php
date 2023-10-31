5ff279520251abd1ba9d544eccce02e716be22c9
muhammad rofi <muhammadrofi062@gmail.com>
2023-10-31 21:31:08 +0700

    rest_api pertemuan5

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +31,8 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 // Method DELETE
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+
+// Route students
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
