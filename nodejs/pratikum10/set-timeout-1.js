ad8ca2c4c9c102f93f585baf14ac403d3d0303d5
muhammad rofi <muhammadrofi062@gmail.com>
2023-12-08 09:29:43 +0700

    pertemuan10- learning asynchronous

---
+++ nodejs/pratikum10/set-timeout-1.js
@@ -0,0 +1,16 @@
+console.log("Satu");
+
+// akan run ketika sudah tiga detik
+setTimeout(function () {
+    console.log("dua");
+}, 3000);
+
+console.log("tiga");
+
+// setTimeout(function () {
+//     console.log("satu detik");
+// }, 1000);
+
+// setTimeout(function () {
+//     console.log("dua detik");
+// }, 2000);
---
+++ nodejs/pratikum10/set-timeout-2.js
@@ -0,0 +1,9 @@
+console.log("Rofi membuka Google Browser");
+
+// akan jalan/muncul ketika sudah 5 detik
+setTimeout(() => {
+    console.log("Downloading 1 Hour...");
+    console.log("Downloading complete");
+}, 5000);
+
+console.log("Rofi mencari file");
